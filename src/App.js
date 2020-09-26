6d74f57c460532e0247517c69dc6afbf7fe2b0d1
jyoti <jshindems@gmail.com>
2020-09-26 12:03:19 -0400

    added expenses component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Balance from "./components/Balance";
+import Expenses from "./components/Expenses"
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <div className = "container">
         <Balance />
+        <Expenses />
       </div>
-    </div>
+    </>
   );
 }
 
