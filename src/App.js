2c6d2ddff98c5d462a52cd84048736b8f2e759c4
jyoti <jshindems@gmail.com>
2021-03-19 20:09:22 -0400

    added header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
- 
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
